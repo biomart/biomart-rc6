f7714db45757317220c4ee7a722a2be651c1411c
Jack Hsu <jack.hsu@oicr.on.ca>
2011-09-30 18:41:58 +0000

    Add missing class. DCCTEST-2131
    git-svn-id: https://code.oicr.on.ca/svn/biomart/biomart-java/branches/release-0_8-candidate_6@10217 8dd1c8d6-c59f-4bb3-b62a-300035dbf9e7

---
+++ src/org/biomart/api/rest/CountEstimate.java
@@ -0,0 +1,13 @@
+package org.biomart.api.rest;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author jhsu
+ */
+@XmlRootElement(name="estimate")
+public class CountEstimate {
+	public int entries;
+	public int total;
+}
